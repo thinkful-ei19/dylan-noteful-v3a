d0290597a9f92e78f2066954126643aa26959fb0
Dylan Stratton <dm.stratton94@gmail.com>
2018-03-22 14:36:14 -0500

    folder tests

--- routes/folders.js
+++ routes/folders.js
@@ -95,6 +95,24 @@ router.put('/folders/:id', (req, res, next) => {
 
 });
 
+router.delete('/folders/:id', (req, res, next) => {
+
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  Folder.findByIdAndRemove(id)
+    .then(result => {
+      result ? res.status(204).end() : next();
+    })
+    .catch(next);
+
+});
+
 
 
 module.exports = router;
---
+++ test/folders.test.js
@@ -0,0 +1,213 @@
+'use strict';
+
+const app = require('../server');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+
+const { TEST_MONGODB_URI } = require('../config');
+
+const Folder = require('../models/folder');
+const seedFolders = require('../db/seed/folders');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Noteful API - Folders', function () {
+
+  before(function () {
+    return mongoose.connect(TEST_MONGODB_URI);
+  });
+
+  beforeEach(function () {
+    return Folder.insertMany(seedFolders);
+  });
+
+  afterEach(function () {
+    return mongoose.connection.db.dropDatabase();
+  });
+
+  after(function () {
+    return mongoose.disconnect();
+  });
+
+  describe('GET /api/folders', function () {
+
+    it('should return the correct number of Folders', function () {
+
+      const dbPromise = Folder.find();
+      const apiPromise = chai.request(app).get('/api/folders');
+
+      return Promise.all([dbPromise, apiPromise])
+        .then(([data, res]) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(data.length);
+          res.body.forEach(function (item) {
+            expect(item).to.be.a('object');
+            expect(item).to.have.keys('id', 'name');
+          });
+        });
+
+    });
+
+  });
+
+  describe('GET /api/folders/:id', function () {
+
+    it('should return correct folder for single ID', function () {
+
+      let data;
+      return Folder.findOne()
+        .then(_data => {
+          data = _data;
+          return chai.request(app).get(`/api/folders/${data.id}`);
+        })
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.have.keys('id', 'name');
+
+          expect(res.body.id).to.equal(data.id);
+          expect(res.body.title).to.equal(data.title);
+          expect(res.body.content).to.equal(data.content);
+        });
+
+    });
+
+    it('should respond with a 400 for an invalid id', function () {
+      const badId = '99-99-99';
+
+      return chai.request(app)
+        .get(`/api/folders/${badId}`)
+        .catch(err => err.response)
+        .then(res => {
+          expect(res).to.have.status(400);
+          expect(res.body.message).to.eq('The `id` is not valid');
+        });
+    });
+
+    it('should respond with a 404 for non-existent id', function () {
+
+      return chai.request(app)
+        .get('/api/folders/AAAAAAAAAAAAAAAAAAAAAAAA')
+        .catch(err => err.response)
+        .then(res => {
+          expect(res).to.have.status(404);
+        });
+    });
+
+  });
+
+  describe('POST /api/folders', function () {
+
+    it('should create a new folder', function () {
+
+      const newFolder = { name: 'New Folder' };
+
+      let res;
+      return chai.request(app)
+        .post('/api/folders')
+        .send(newFolder)
+        .then(function (_res) {
+          res = _res;
+          expect(res).to.have.status(201);
+          expect(res).to.have.header('location');
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.have.keys('id', 'name');
+          return Folder.findById(res.body.id);
+        })
+        .then(data => {
+          expect(res.body.name).to.equal(data.name);
+        });
+
+    });
+
+    it('should return an error when posting a folder without a `name` field', function () {
+
+      const newFolder = { 'hello': 'goodbye' };
+
+      return chai.request(app)
+        .post('/api/folders')
+        .send(newFolder)
+        .catch(err => err.response)
+        .then(res => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.equal('Missing `name` in request body');
+        });
+
+    });
+
+  });
+
+  describe('PUT /api/folders/:id', function () {
+
+    it('should update the folder', function () {
+
+      const updateFolder = { name: 'Update Folder' };
+
+      let data;
+      return Folder.findOne()
+        .then(_data => {
+          data = _data;
+          return chai.request(app)
+            .put(`/api/folders/${data.id}`)
+            .send(updateFolder);
+        })
+        .then(function (res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.have.keys('id', 'name');
+
+          expect(res.body.id).to.equal(data.id);
+          expect(res.body.name).to.equal(updateFolder.name);
+        });
+
+    });
+
+    it('should return an error when missing `name` field', function () {
+
+      const updateFolder = { 'hello': 'goodbye' };
+
+      return chai.request(app)
+        .put('/api/folders/9999')
+        .send(updateFolder)
+        .catch(err => err.response)
+        .then(res => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.equal('Missing `name` in request body');
+        });
+
+    });
+
+  });
+
+  describe('DELETE /api/notes/:id', function () {
+
+    it('should delete a folder by id', function () {
+
+      let data;
+      return Folder.findOne()
+        .then(_data => {
+          data = _data;
+          return chai.request(app).delete(`/api/folders/${data.id}`);
+        })
+        .then(function (res) {
+          expect(res).to.have.status(204);
+        });
+
+    });
+
+  });
+
+});
